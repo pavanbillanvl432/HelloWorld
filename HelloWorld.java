df2516c690761f2278084e4b357bf1a803b7cadd
Tiger@432 <pavan_bnvl@yahoo.com>
2018-11-18 09:22:48 +1100

    modified Hello world commiting

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,45 @@
 package hello;
 
 public class HelloWorld {
-	public static void main(String args[]) {
-		System.out.println("Hello World");
+//	private static int first(int a,int b) {
+//		if(b==0) {
+//			return a;
+//		}
+//		else {
+//			return second(b, a-b);
+//		}
+//	}
+//	
+//		private static int second(int a,int b) {
+//			if (a==0) {
+//				return b;
+//			}
+//			else {
+//				return first(b,a);
+//			}
+//			
+//		}
 		
-	}
-
+		public static void main(String args[]) {
+//			Object obj1=new Object();
+//			Object obj2=new Object();
+//			System.out.println("result: ");
+//			System.out.println((obj1==obj2)+" ");
+//			System.out.println(obj1.equals(obj2)+" ");
+//			obj1=obj2;
+//			System.out.println(obj1.equals(2)+"");
+//			
+//		}
+	
+	
+//		
+int i=1;
+if(i++==--i) {
+	System.out.println("0");
+}
+else {
+	System.out.println("1");
+}
+	
+}
 }
